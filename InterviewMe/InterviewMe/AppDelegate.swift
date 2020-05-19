537679ac1013a5d6b775ca4031d6d2ebe70c2160
alexpaul <alexpaulnyc@gmail.com>
2020-05-18 20:46:42 -0400

    configure app for audio playback

--- InterviewMe/InterviewMe/AppDelegate.swift
+++ InterviewMe/InterviewMe/AppDelegate.swift
@@ -7,14 +7,19 @@
 //
 
 import UIKit
+import AVFoundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    let audioSession = AVAudioSession.sharedInstance()
+    do {
+      try audioSession.setCategory(.playback)
+    } catch {
+      print("failed playback")
+    }
     return true
   }
 
